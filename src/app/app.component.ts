be78e4417f1c3f083884a2234ab42ee67ae3cd7f
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-09-08 10:39:42 +0200

    working, no model added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./product-list/product-list.component";
-
+import { ProductService } from './product.service';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductListComponent],
+  imports: [CommonModule, RouterOutlet, ProductListComponent, HttpClientModule ],
+  providers: [ProductService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import * as productsData from './products.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  data = productsData;
 
-  constructor() { }
+  constructor() {}
+
+  getProducts(): Observable<any> {
+    return of(this.data.products);
+  }
 }
